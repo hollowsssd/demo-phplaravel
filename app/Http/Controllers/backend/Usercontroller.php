6d9072552f9efdbfd0f61616db4cfcb9acaa2a70
hollowsssd <131234928+hollowsssd@users.noreply.github.com>
2024-11-21 17:18:27 +0700

    demo-phplaravel

---
+++ app/Http/Controllers/backend/Usercontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Usercontroller extends Controller
+{
+    public function __construct(){
+
+    }
+
+}
